71f8eabdf3ef1caf6a44b29f93f6624d1dd58a09
Ezgi Özen <ezg.ozen@hotmail.com>
2015-05-20 23:17:17 +0300

    hotel

---
+++ hotelmanage/hotelmanage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(hotelmanage.Startup))]
+namespace hotelmanage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
